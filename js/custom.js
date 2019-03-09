27ac06def2df991c146a3c0e210bd4ec34f38d1d
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2019-03-09 23:10:08 +0100

    JavaScript : Close navigatin bar if clicked somewhere on the page

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,15 @@ $(document).ready(function() { //when document(DOM) loads completely.
     }
   });
 });
+
+/*========== CLOSE MOBILE NAV ON CLICK ==========*/
+
+$(document).ready(function () { //when document loads completely.
+  $(document).click(function (event) { //click anywhere
+      var clickover = $(event.target); //get the target element where you clicked
+      var _opened = $(".navbar-collapse").hasClass("show"); //check if element with 'navbar-collapse' class has a class called show. Returns true and false.
+      if (_opened === true && !clickover.hasClass("navbar-toggler")) { // if _opened is true and clickover(element we clicked) doesn't have 'navbar-toggler' class
+          $(".navbar-toggler").click(); //toggle the navbar; close the navbar menu in mobile.
+      }
+  });
+});
